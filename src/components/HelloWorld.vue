f525ee49e18c7d629c846ec537314977b52f21c6
= <f.f.nascimento9@gmail.com>
2019-07-15 02:48:59 -0400

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,77 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <cv-file-uploader
+      :label="fileUploaderLabel"
+      :helperText="helperText"
+      :buttonLabel="fileUploaderButtonLabel"
+      :accept="fileUploaderAccept"
+      :clear-on-reselect="fileUploaderClearOnReselect"
+      :initial-state-uploading="fileUploaderInitialStateUploading"
+      :multiple="fileUploaderMultiple"
+      :removable="fileUploaderRemovable"
+      @change="onChange" 
+      v-model="storyFiles" ref="fileUploader">
+    </cv-file-uploader>
+    <cv-button @click="transcribe" :disabled="!storyFiles">Transcribe</cv-button>
+    <cv-text-area
+      :label="transcriptionLabel"
+      v-model="transcription">
+    </cv-text-area>
+
+    <cv-loading v-if="loading" :active="loading" :overlay="overlay"></cv-loading>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  data: () => ({
+    transcriptionLabel: "Transcription",
+    fileUploaderLabel: "Select an audio file",
+    storyFiles: null,
+    helperText: "",
+    fileUploaderButtonLabel: "Add File",
+    fileUploaderAccept: "",
+    fileUploaderClearOnReselect: true,
+    fileUploaderInitialStateUploading: false,
+    fileUploaderMultiple: false,
+    transcription: "",
+    fileUploaderRemovable: true,
+    loading: false,
+    overlay: true
+  }),
+  methods: {
+    onChange() {
+      console.log(this.storyFiles)
+    },
+    transcribe() {
+      if (this.storyFiles) {
+        let formData = new FormData();
+        formData.append('file', this.storyFiles[0].file)
+        this.loading = true;
+        axios.post('/transcribe', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }).then(res => {
+            this.loading = false;
+            console.log(res)
+            res.data.results.forEach(text => {
+              console.log(text.alternatives[0].transcript)
+              this.transcription += text.alternatives[0].transcript + '\n'
+            })
+            console.log(this.transcription)
+          })
+          .catch(err => {
+            console.log('FAILURE!!');
+            console.log(err)
+          });
+      }
+    }
+  },
   name: 'HelloWorld',
   props: {
     msg: String
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'carbon-components/css/carbon-components.css';
+import CarbonComponentsVue from '@carbon/vue/src/index';
+Vue.use(CarbonComponentsVue);
+
 Vue.config.productionTip = false
 
 new Vue({
